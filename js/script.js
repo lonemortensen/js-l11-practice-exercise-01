57c94e1e52ceb0a1eabf41d5a7be79ba4aa7620b
Lone Mortensen <lmordev@gmail.com>
2022-01-21 17:05:16 -0800

    Added an async function to fetch API data and a function to randomly select user info in js file. Created drop-down list in js file, and removed hide class in html file

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
+/*PROJECT: RANDOM USER GENERATOR*/
+
 const randomFolks = document.querySelector(".random-peeps");
+const selectUserNumber = document.querySelector("#users");
+
+
+/*Get user data from API:*/
+const getData = async function (numUsers) {
+    const usersRequest = await fetch (`https://randomuser.me/api?results=${numUsers}`);
+    const data = await usersRequest.json();
+    console.log(data);
+    const userResults = data.results;
+    displayUsers(userResults);
+}; 
+
+getData(1);
+
+/*Display user data on webpage:*/
+const displayUsers = function (userResults) {
+    randomFolks.innerHTML = ""; //Clear the randomFolks element
+    for (let user of userResults) {
+        let country = user.location.country;
+        let name = user.name.first;
+        let imageURL = user.picture.medium;
+        let userDiv = document.createElement("div");
+        userDiv.innerHTML = `
+            <h3>${name}</h3>
+            <p>${country}</p>
+            <img src=${imageURL} alt="User avatar"/>
+        `;
+        randomFolks.append(userDiv);
+    }
+};
+ /*Drop-down list to change number of user data (profiles) displayed:*/
+selectUserNumber.addEventListener("change", function(e) { //selectUserNumber is the target of the dropdown list
+    const numUsers = e.target.value;
+    getData(numUsers);
+});
